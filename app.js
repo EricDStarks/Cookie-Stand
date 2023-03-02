90adf2d06518c98cdf2175e6a2355109e0b84a9d
Eric Starks <123973291+EricDStarks@users.noreply.github.com>
2023-03-02 13:51:08 -0600

    app

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ let hoursArray = document.querySelector('.output')
     for (let i = 0; i < seattle.hoursOfOperation.length; i++){
         let seattlecookies = document.createElement("li");
         seattlecookies.innerHTML = `${seattle.hoursOfOperation[i]}: ${seattle.cookiesPerHour[i]}: cookies`;
-        hoursArray.append(seattlecookies);
+        hoursArray.append(getCookies);
     }
 
 // calls each function so array of cookies is put into an array. If the function isn't called it'll be undefined.
