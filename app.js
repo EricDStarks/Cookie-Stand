61e3340d1cc0b1dc490f219ad02e2f85826056e4
Eric Starks <123973291+EricDStarks@users.noreply.github.com>
2023-03-02 11:46:15 -0600

    app

--- app.js
+++ app.js
@@ -121,3 +121,11 @@ seattle.getCookies();
     }
     // call for lima function
     lima.getCookies();
+
+    let hoursArray = document.querySelector('.ouptut')
+    for (let i = 0; i < seattle.hoursOfOperation.length; i++){
+        let variable = document.createElement("li");
+        variable.innerHTML = `${seattle.hoursofOperation[i]}: ${seattle.cookiesPerHour[i]}: cookies | ${tokyo.hoursOfOperation[i]}: ${tokyo.cookiesPerHour[i]}: cookies | ${dubai.hoursOfOperation[i]}: ${dubai.cookiesPerHour[i]}: cookies | ${paris.hoursOfOperation[i]}: ${paris.cookiesPerHour[i]}: cookies | ${lima.hoursOfOperation[i]}: ${lima.cookiesPerHour[i]} cookies `;
+        hoursArray.append(variable);
+    }
+    
