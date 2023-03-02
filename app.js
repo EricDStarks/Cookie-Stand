10b7ef6b221e534d72c6fdde670577bdd4bfc42a
Eric Starks <123973291+EricDStarks@users.noreply.github.com>
2023-03-02 10:53:53 -0600

    html

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-'use strict'
 //random function between max and min input
 function getRandom(max, min) {
-    return Math.ceil(Math.random() * (max - min) + min);
-    // 0 >= random #  <= 1  * limit
-}
+    let random = Math.random() * (max - min) + min;
 
+    return random;
+    
+}
+// Objects with the min, max, and avg as properties for each city
 const seattle = {
     min: 23,
     max: 65,
@@ -16,103 +17,107 @@ const seattle = {
         console.log(this.min);
         return getRandom(this.max, this.min);
     },
-    cookiesPerHour: [],
+    cookiesPerHour:[],
     getCookies: function() {
         for(let i = 0; i < this.hoursOfOperation.length; i++) {
-            console.log('seattle avg cookies/sale', this.avg);
-            console.log('seattle avg customers/hour', this.customersPerHour());
+        //console.log('this.avg', this.avg)
+        //console.log('this.customerperhour()', this.customersPerHour());
             this.cookiesPerHour.push(Math.ceil(this.avg * this.customersPerHour()));
         }
         return this.cookiesPerHour;
-    },
+    }
     
 }
-//object definition for a location
-    //---starting data
-        /*
-        Location	Min/Cust	Max/Cust	AvgCookie/Sale
-        Seattle	    23	        65	        6.3
-        */
-       
-    //----customers per hour
-        /*2. Use a method of that object to generate a 
-        random number of customers per hour. */
+// calls each function so array of cookies is put into an array. If the function isn't called it'll be undefined.
+seattle.getCookies();
 
-    //---cookies per hour
-        /* 3. Calculate and store the simulated amounts of cookies 
-        purchased for each hour at each location using average 
-        cookies purchased and the random number of customers generated.*/
-
-    //customers per hour
-        /* 4. Store the results for each location in a separate array… 
-        perhaps as a property of the object representing that location. */
+//object definition for each location
     
-
-// 5. Display the values of each array as unordered lists in the browser.
-    /*
-    Calculating the sum of these hourly totals; your output for each location should look like this:
-*/
-let hoursDemo = ["6am", "7am", "8am", "9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm", "6pm", "7pm"];
-    let cookieDemo = [152,265,202,353,227,221,202,378,171,309,215,189,189,385];
-    let hoursArray = document.querySelector('.output')
-    for(let i = 0; i < hoursDemo.length; i++) {
-        //hoursDemo[i].innerHTML
-        let variable = document.createElement("li");
-        variable.innerHTML = `${hoursDemo[i]}: ${cookieDemo[i]} cookies`;
-        hoursArray.append(variable);
-    }
-     /*
-    Seattle
-
-    6am: 16 cookies
-    7am: 20 cookies
-    8am: 35 cookies
-    9am: 48 cookies
-    10am: 56 cookies
-    11am: 77 cookies
-    12pm: 93 cookies
-    1pm: 144 cookies
-    2pm: 119 cookies
-    3pm: 84 cookies
-    4pm: 61 cookies
-    5pm: 23 cookies
-    6pm: 42 cookies
-    7pm: 57 cookies
-    Total: 875 cookies
-    */
-    function getRandom(max, min) {
-        return Math.ceil(Math.random() * (max - min) + min);
-        // 0 >= random #  <= 1  * limit
-    }
     
-    const tokyo = {
+  const tokyo = {
         min: 3,
         max: 24,
         avg: 1.2,
-        location: 'Tokyo',
         hoursOfOperation: ["6am", "7am", "8am", "9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm", "6pm", "7pm"],
         customersPerHour: function() {
-            console.log(this.max);
-            console.log(this.min);
             return getRandom(this.max, this.min);
         },
         cookiesPerHour: [],
         getCookies: function() {
             for(let i = 0; i < this.hoursOfOperation.length; i++) {
-                console.log('tokyo avg cookies/sale', this.avg);
-                console.log('tokyo avg customers/hour', this.customersPerHour());
-                this.cookiesPerHour.push(Math.ceil(this.avg * this.customersPerHour()));
+                console.log('this.avg', this.avg)
+                console.log('this.customersperhour()', this.customersPerHour());
+                let cookies = Math.ceil(this.avg * this.customersPerHour())
             }
             return this.cookiesPerHour;
-        },
+        }
         
     }
-    let hoursDemo0 = ["6am", "7am", "8am", "9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm", "6pm", "7pm"];
-    let cookieDemo0 = [152,265,202,353,227,221,202,378,171,309,215,189,189,385];
-    let newhoursArray = document.querySelector('.output')
-    for(let i = 0; i < hoursDemo.length; i++) {
-        //hoursDemo[i].innerHTML
-        let variable = document.createElement("li");
-        variable.innerHTML = `${hoursDemo[i]}: ${cookieDemo[i]} cookies`;
-        hoursArray.append(variable);
+    // call tokyo function
+    tokyo.getCookies();
+
+    const dubai = {
+        min:11,
+        max:38,
+        avg:3.7,
+        hoursOfOperation:["6am","7am","8am","9am","10am","11am","12am","1pm","2pm","3pm","4pm","5pm","6pm","7pm"],
+        customersPerHour: function() {
+            return getRandom(this.max, this.min)
+        },
+        cookiesPerHour:[],
+        getCookies: function() {
+            for (i = 0; i < this.hoursOfOperation.length; i++) {
+            console.log("this.avg", this.avg)
+            console.log("this.customersperhour()", this.customersPerHour())
+            let cookies = Math.ceil(this.avg * this.customersPerHour())
+            this.cookiesPerHour.push(cookies)
+            }
+             return this.cookiesPerHour
+        }
     }
+    // call for dubai function
+    dubai.getCookies();
+
+    const paris = {
+        min:20,
+        max:38,
+        avg:2.3,
+        hoursOfOperation:["6am","7am","8am","9am","10am","11am","12am","1pm","2pm","3pm","4pm","5pm","6pm","7pm"],
+        customersPerHour: function() {
+            return getRandom(this.max, this.min)
+        },
+        cookiesPerHour:[],
+        getCookies: function() {
+            for (i = 0; i < this.hoursOfOperation.length; i++) {
+            console.log("this.avg", this.avg)
+            console.log("this.customersperhour()", this.customersPerHour())
+            let cookies = Math.ceil(this.avg * this.customersPerHour())
+            this.cookiesPerHour.push(cookies)
+            }
+             return this.cookiesPerHour
+        }
+    }
+    // call for paris function
+    paris.getCookies();
+
+    const lima = {
+        min:2,
+        max:16,
+        avg:4.6,
+        hoursOfOperation:["6am","7am","8am","9am","10am","11am","12am","1pm","2pm","3pm","4pm","5pm","6pm","7pm"],
+        customersPerHour: function() {
+            return getRandom(this.max, this.min)
+        },
+        cookiesPerHour:[],
+        getCookies: function() {
+            for (i = 0; i < this.hoursOfOperation.length; i++) {
+            console.log("this.avg", this.avg)
+            console.log("this.customersperhour()", this.customersPerHour())
+            let cookies = Math.ceil(this.avg * this.customersPerHour())
+            this.cookiesPerHour.push(cookies)
+            }
+             return this.cookiesPerHour
+        }
+    }
+    // call for lima function
+    lima.getCookies();
